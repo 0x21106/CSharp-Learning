55f3d4d4fc60b16cc7845b6d4581ac732c1328f6
0x21106 <ayhan2015007@gmail.com>
2020-03-06 17:31:48 +0400

    new things

---
+++ CSharpCourse/Classes/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Customer
+    {
+        public int id { get; set; }
+
+        private string __firstName;
+        public string firstName {
+            get
+            {
+                return "Mr. " + __firstName;
+            } 
+            set
+            {
+                __firstName = value;
+            } 
+        }
+        public string lastName { get; set; }
+        public string city { get; set; }
+    }
+}
---
+++ CSharpCourse/Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.city = "Baku";
+            customer.id = 1;
+            customer.firstName = "Ayxan";
+            customer.lastName = "Abdullayev";
+
+            Customer customer2 = new Customer
+            {
+                id = 2,
+                city = "Gence",
+                firstName = "Tuziq",
+                lastName = "Paltaryuyan"
+            };
+
+            //Console.WriteLine(customer.firstName);
+            Console.WriteLine(customer2.firstName);
+
+            Console.ReadKey();
+        }
+    }
+}
